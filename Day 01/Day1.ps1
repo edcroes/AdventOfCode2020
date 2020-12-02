a05ac4b0891c5b2aed89a80531d5c17c12813bf8
Edwin Croes <ec.croes@belastingdienst.nl>
2020-12-02 12:03:05 +0100

    Added day 1 and 2 solutions

---
+++ Day 01/Day1.ps1
@@ -0,0 +1,37 @@
+﻿$items = Get-Content -Path "$PSScriptRoot\input.txt" | ForEach-Object { [int]::Parse($_) } | Select-Object -Unique | Sort-Object
+
+function FindItemsThatMatchUp
+{
+    param($Items, $Number)
+
+    foreach ($item in $Items)
+    {
+        $searchFor = $Number - $item
+
+        if ($Items -contains $searchFor)
+        {
+            return @($item, $searchFor)
+        }
+    }
+
+    return $null
+}
+
+# Part 1
+$result = FindItemsThatMatchUp $items 2020
+Write-Output "Answer part 1: $($result[0]) * $($result[1]) = $($result[0] * $result[1])"
+
+
+# Part 2
+foreach ($item in $items)
+{
+    $searchFor = 2020 - $item
+
+    $result = FindItemsThatMatchUp -Items $items -Number $searchFor
+
+    if ($result)
+    {
+        Write-Output "Answer part 2: $item * $($result[0]) * $($result[1]) = $($item * $result[0] * $result[1])"
+        break
+    }
+}
---
+++ Day 02/Day2.ps1
@@ -0,0 +1,56 @@
+﻿$PasswordRegex = [regex]"^(\d+)-(\d+) ([a-z]): ([a-z]+)$"
+$lines = Get-Content -Path "$PSScriptRoot\input.txt" | Where-Object { $_ }
+
+function ParseLine
+{
+    param([string] $Line)
+
+    $matches = $PasswordRegex.Match($Line)
+    return [PSCustomObject] @{
+        Min = [int]::Parse($matches.Groups[1].Value)
+        Max = [int]::Parse($matches.Groups[2].Value)
+        Letter = $matches.Groups[3].Value
+        Password = $matches.Groups[4].Value
+    }
+}
+
+function IsValidPart1
+{
+    param($PasswordInfo)
+
+    $chars = $PasswordInfo.Password.ToCharArray() | Where-Object { $_ -eq $PasswordInfo.Letter }
+    
+    return $chars.Count -ge $PasswordInfo.Min -and $chars.Count -le $PasswordInfo.Max
+}
+
+function IsValidPart2
+{
+    param($PasswordInfo)
+
+    $chars = $PasswordInfo.Password.ToCharArray()
+    
+    return $chars[$PasswordInfo.Min-1] -eq $PasswordInfo.Letter -xor $chars[$PasswordInfo.Max-1] -eq $PasswordInfo.Letter
+}
+
+$validPart1Count = 0
+$validPart2Count = 0
+
+foreach ($line in $lines)
+{
+    $pw = ParseLine -Line $line
+    $isValidPart1 = IsValidPart1 -PasswordInfo $pw
+    $isValidPart2 = IsValidPart2 -PasswordInfo $pw
+
+    if ($isValidPart1)
+    {
+        $validPart1Count++
+    }
+
+    if ($isValidPart2)
+    {
+        $validPart2Count++
+    }
+}
+
+Write-Output "Answer Part 1: $($validPart1Count)"
+Write-Output "Answer Part 2: $($validPart2Count)"
