13b9043b4aab07489ce540c0dcd2ac1585ac278d
Edwin Croes <ed_croes@hotmail.com>
2020-12-05 12:17:23 +0100

    Day 5

---
+++ Day 05/Day5.ps1
@@ -0,0 +1,24 @@
+function GetSeatId
+{
+    param([string]$Line)
+
+    $row = [Convert]::ToUInt16($Line.Substring(0, 7).Replace("B", "1").Replace("F", "0"), 2)
+    $seat = [Convert]::ToByte($Line.Substring(7).Replace("L", "0").Replace("R", "1"), 2)
+    return $row * 8 + $seat
+}
+
+$seats = Get-Content -Path "$PSScriptRoot\input.txt" | Foreach-Object { GetSeatId -Line $_ }
+
+# Part 1
+# Actually answered this one by just searching the file, start with BBBBBBB etc till found
+Write-Host "Answer Part 1: $(($seats | Sort-Object -Descending)[0])"
+
+# Part 2
+foreach ($seat in ($seats | Sort-Object))
+{
+    if ($seats -notcontains $seat + 1 -and $seats -contains $seat + 2)
+    {
+        Write-Host "Answer Part 2: $($seat + 1)"
+        break
+    }
+}
