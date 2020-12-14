d3fba0675fefac7b7f6f478abd476aa4a73805c7
Edwin Croes <ed_croes@hotmail.com>
2020-12-14 16:30:47 +0100

    Better comments for Day 13

--- Day 13/Day13-Part2.ps1
+++ Day 13/Day13-Part2.ps1
@@ -20,54 +20,56 @@ function GetInverseNi
 }
 
 <#
-x = bi(mod ni) --> bi is the remainder
+x ≡ bᵢ(mod nᵢ) --> bᵢ is the remainder
 --------------
-x = b1(mod n1)
-x = b2(mod n2)
-x = b3(mod n3)
+x ≡ b₁(mod n₁)
+x ≡ b₂(mod n₂)
+x ≡ b₃(mod n₃)
 
-M = n1n2n3
-Mi = M / ni
-xi = Inverse of Mi
+M = n₁n₂n₃
+Mᵢ = M / nᵢ
+xᵢ = Inverse of Mᵢ
 
-Find Inverse of Mi:
-Mixi = 1(mod ni)
-(Mi%ni)xi = 1(mod ni)
-Determine for which (Mi%ni)xi the remainder of dividing by ni equals 1
+Find Inverse of Mᵢ:
+Mᵢxᵢ ≡ 1(mod nᵢ)
+(Mᵢ%nᵢ)xᵢ ≡ 1(mod nᵢ)
+Determine for which (Mᵢ%nᵢ)xᵢ the remainder of dividing by nᵢ equals 1
 
-bi  Mi  xi  biMixi
+bᵢ  Mᵢ  xᵢ  bᵢMᵢxᵢ
 ------------------
-b1  M1  x1  b1M1x1
-b2  M2  x2  b2M2x2
-b3  M3  x3  b3M3x3
+b₁  M₁  x₁  b₁M₁x₁
+b₂  M₂  x₂  b₂M₂x₂
+b₃  M₃  x₃  b₃M₃x₃
 
-x = sum of biMixi (mod M)
+x ≡ sum of bᵢMᵢxᵢ (mod M)
 
-Final answer = (sum of biMixi) % M
+Final answer = (sum of bᵢMᵢxᵢ) % M
 
 Example
 --------
-x = 3(mod 5)
-x = 1(mod 7)
-x = 6(mod 8)
+x ≡ 3(mod 5)
+x ≡ 1(mod 7)
+x ≡ 6(mod 8)
 
-M = 5 x 7 x 8 = 280
+M = 5 * 7 * 8 = 280
 
-bi  Mi  xi    biMixi
-------------------
-3   56  1 [a] 3 x 56 x 1 = 168
-1   40  3 [b] 1 x 40 x 3 = 120
-5   35  3 [c] 5 x 35 x 3 = 630
+bᵢ  Mᵢ  xᵢ      bᵢMᵢxᵢ
+---------------------------------
+3   56  1 [a]   3 * 56 * 1 = 168
+1   40  3 [b]   1 * 40 * 3 = 120
+5   35  3 [c]   5 * 35 * 3 = 630
 
-[a] 56x1 = 1(mod 5) -> 56 % 5 = 1 -> 1x1 = 1(mod 5) -> x1 = 1
-[b] 40x2 = 1(mod 7) -> 40 % 7 = 5 -> 5x2 = 1(mod 7) -> 5 x 3 = 15, 15 % 7 = 1 -> x2 = 3
-[c] 35x3 = 1(mod 8) -> 35 % 8 = 3 -> 3x3 = 1(mod 8) -> 3 x 3 = 9, 9 % 8 = 1 -> x3 = 3
+[a] 56x₁ ≡ 1(mod 5) -> 56 % 5 = 1 -> 1x₁ ≡ 1(mod 5) -> x₁ = 1
+[b] 40x₂ ≡ 1(mod 7) -> 40 % 7 = 5 -> 5x₂ ≡ 1(mod 7) -> 5 * 3 = 15, 15 % 7 = 1 -> x₂ = 3
+[c] 35x₃ ≡ 1(mod 8) -> 35 % 8 = 3 -> 3x₃ ≡ 1(mod 8) -> 3 * 3 = 9, 9 % 8 = 1 -> x₃ = 3
 
-x = Sum of biMixi = 168 + 120 + 630 = 918
-x = 918(mod 280) -> x = 78(mod 280)
+x = Sum of bᵢMᵢxᵢ = 168 + 120 + 630 = 918
+x ≡ 918(mod 280) -> answer:  x ≡ 78(mod 280)
 918 % 280 = 78
 
-So the answer is 78
+In this case the answer that is returned is 78.
+
+Explanation used: https://www.youtube.com/watch?v=zIFehsBHB8o
 #>
 function ChineseRemainder
 {
